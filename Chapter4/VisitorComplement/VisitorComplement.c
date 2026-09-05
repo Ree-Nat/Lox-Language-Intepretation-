b33a72fd2762cb0836601f1e26fe78aa4eec296f
Nathan <53132424+Ree-Nat@users.noreply.github.com>
2026-09-05 15:41:19 -0700

    add additional comment, remove main function

--- Chapter4/VisitorComplement/VisitorComplement.c
+++ Chapter4/VisitorComplement/VisitorComplement.c
@@ -1,10 +1,37 @@
-//bundling types with stuct
+//bundling types 
+
+//so opposite would be bundling all variants of a signle type together in a data definition
+
+
+// Circle - > Draw(), Area()
+// Rectangle -> Draw(), Area()
+      
 #include <stdio.h>
+struct Circle {
+    double radius;
+};
 
-typedef struct{
-    char name[50]
-    int age;
+double circle_area(struct Circle c)
+{
+    return 3.14 * c.radius * c.radius;
+}
 
-} Example
+void circle_draw(struct Circle c)
+{
+    printf("Drawing circle with radius %.1f\n", c.radius);
+}
 
+struct Rectangle {
+    double width;
+    double height;
+};
+double rectangle_area(struct Rectangle r)
+{
+    return r.width * r.height;
+}
 
+void rectangle_draw(struct Rectangle r)
+{
+    printf("Drawing rectangle %.1f x %.1f\n",
+           r.width, r.height);
+}
